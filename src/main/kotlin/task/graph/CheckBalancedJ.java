25c7f3ff8c154492c153c685c28424bbeff06698
Karol Pietryka <karol.pietryka11@gmail.com>
2023-04-07 18:27:02 +0200

    [Improvement]Improve CheckBalancedJ to O(n)

--- src/main/kotlin/task/graph/CheckBalancedJ.java
+++ src/main/kotlin/task/graph/CheckBalancedJ.java
@@ -8,21 +8,20 @@ import java.util.Optional;
 //this question, a balanced tree is defined to be a tree such that the heights of the two subtrees of any
 //node never differ by more than one.
 public class CheckBalancedJ {
+    //Time complexity O(N)
     public Boolean isBalanced(TreeNode<Integer> root){
         if (root == null) {
             return true;
         }
         int leftDepth = getDepth(root.getLeft());
         int rightDepth = getDepth(root.getRight());
-        if (Math.abs(leftDepth - rightDepth) > 1) {
-            return false;
-        }
-        return isBalanced(root.getLeft()) && isBalanced(root.getRight());    }
+        return Math.abs(leftDepth - rightDepth) <= 1;
+    }
     private int getDepth(TreeNode<Integer> node){
         if(node == null) { return 0; }
-        return Math.max(
-            Optional.ofNullable(node.getLeft()).map(this::getDepth).orElse(0),
-            Optional.ofNullable(node.getRight()).map(this::getDepth).orElse(0))
-        + 1;
+        int leftNodeDepth = Optional.ofNullable(node.getLeft()).map(this::getDepth).orElse(0);
+        int rightNodeDepth = Optional.ofNullable(node.getRight()).map(this::getDepth).orElse(0);
+        if(Math.abs(leftNodeDepth - rightNodeDepth) > 1){ return Integer.MIN_VALUE; }
+        else { return Math.max(leftNodeDepth, rightNodeDepth) + 1; }
     }
 }
